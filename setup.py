e2bcfbe827e5d008355b9b9de21bbe534df0c3d8
Lovmong <1967471283@qq.com>
2024-03-01 16:49:47 +0800

    add --break-system-packages for pip3, optimize

--- setup.py
+++ setup.py
@@ -1,21 +1,63 @@
+#!/usr/bin/env python3
+from version import __version__
+from os import geteuid, getlogin
+import sys
+import time
+import threading
 
-errors = []
+if geteuid() != 0:
+    print("Script must be run as root. Try 'sudo python3 setup.y'")
+    sys.exit(1)
 
+
+# utils
+# =================================================================
 def run_command(cmd=""):
     import subprocess
-    p = subprocess.Popen(
-        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen(cmd,
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
     result = p.stdout.read().decode('utf-8')
     status = p.poll()
-    # print(result)
-    # print(status)
     return status, result
 
+
+errors = []
+at_work_tip_sw = False
+
+
+def working_tip():
+    char = ['/', '-', '\\', '|']
+    i = 0
+    global at_work_tip_sw
+    while at_work_tip_sw:
+        i = (i + 1) % 4
+        sys.stdout.write('\033[?25l')  # cursor invisible
+        sys.stdout.write('%s\033[1D' % char[i])
+        sys.stdout.flush()
+        time.sleep(0.5)
+
+    sys.stdout.write(' \033[1D')
+    sys.stdout.write('\033[?25h')  # cursor visible
+    sys.stdout.flush()
+
+
 def do(msg="", cmd=""):
-    print(" - %s..." % (msg), end='\r')
-    print(" - %s... " % (msg), end='')
-    status, result = eval(cmd)
+    print(" - %s... " % (msg), end='', flush=True)
+    # at_work_tip start
+    global at_work_tip_sw
+    at_work_tip_sw = True
+    _thread = threading.Thread(target=working_tip)
+    _thread.daemon = True
+    _thread.start()
+    # process run
+    status, result = run_command(cmd)
     # print(status, result)
+    # at_work_tip stop
+    at_work_tip_sw = False
+    _thread.join()  # wait for thread to finish
+    # status
     if status == 0 or status == None or result == "":
         print('Done')
     else:
@@ -23,55 +65,126 @@ def do(msg="", cmd=""):
         errors.append("%s error:\n  Status:%s\n  Error:%s" %
                       (msg, status, result))
 
+
+def check_devices():
+    with open('/proc/device-tree/model', 'r') as f:
+        model = f.read()
+        if 'Raspberry Pi' in model:
+            return "Raspberry Pi"
+        elif 'Jetson' in model:
+            return "Jetson"
+        else:
+            return model
+
+
+# Dependencies
+# =================================================================
+APT_INSTALL_LIST = [
+    "python3-pip",
+    "sysstat",  # System performance tools for the Linux operating system
+    "i2c-tools",
+    # "raspi-config",
+    "python3-pil",
+    "python3-numpy",
+]
+
+PIP_INSTALL_LIST = [
+    # 'gpiozero',
+    'smbus2',
+    'psutil',
+    "spidev",
+]
+
+model = check_devices()
+if model == 'Raspberry Pi':
+    PIP_INSTALL_LIST.append('gpiozero')
+elif model == 'Jetson':
+    PIP_INSTALL_LIST.append('Jetson.GPIO')
+else:
+    print(f'Unsupported model: {model}')
+    sys.exit(1)
+
+
 def install():
+    user_name = getlogin()
+    print(f'NAS-Kit installation {__version__} on {model} for {user_name}:')
 
-    ### Rpi_epd_lib    
-    do(msg="wget BCM2835",
-        cmd='run_command("wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz")')
-    do(msg="tar and make install BCM2835",
-        cmd='run_command("tar zxvf bcm2835-1.60.tar.gz && cd bcm2835-1.60/ && sudo ./configure && sudo make && sudo make check && sudo make install")')
-    # do(msg="install wiringpi",
-    #     cmd='run_command("sudo apt-get install wiringpi && cd /tmp && wget https://project-downloads.drogon.net/wiringpi-latest.deb && sudo dpkg -i wiringpi-latest.deb")')
-    do(msg="apt-get update",
-        cmd='run_command("sudo apt-get update")')
-    do(msg="apt-get install python3-pip",
-        cmd='run_command("sudo apt-get install python3-pip -y")')
-    do(msg="apt-get install sysstat",
-        cmd='run_command("sudo apt-get install sysstat -y")')
-    do(msg="apt-get install python3-pil",
-        cmd='run_command("sudo apt-get install python3-pil -y")')
-    do(msg="apt-get install python3-numpy",
-        cmd='run_command("sudo apt-get install python3-numpy -y")')
-    do(msg="pip3 install RPi.GPIO",
-        cmd='run_command("sudo pip3 install RPi.GPIO")')
-    do(msg="pip3 install spidev",
-        cmd='run_command("sudo pip3 install spidev")')
+    if "--no-dep" not in sys.argv:
+        ### Rpi_epd_lib
+        do(msg="wget BCM2835",
+           cmd='wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz'
+           )
+        do(msg="tar and make install BCM2835",
+           cmd=
+           'tar zxvf bcm2835-1.60.tar.gz'
+           +' && cd bcm2835-1.60/'\
+           +' && ./configure'\
+           +' && make'\
+           +' && make check '\
+           +' && make install'
+           )
 
-### install epd_code
-    # do(msg="install nas-kit",
-    #     cmd='run_command("cd ~")')
+        # =============================
+        print("Install dependencies with apt-get:")
+        # update apt-get
+        do(msg="update apt-get", cmd='sudo apt-get update')
+        #
+        for dep in APT_INSTALL_LIST:
+            do(msg=f"install {dep}", cmd=f'sudo apt-get install {dep} -y')
 
-### Setup OMV env and install OMV
-    # do(msg="Enter-file",
-    #     cmd='run_command("cd ~/nas-kit/file")') 
-    # do(msg="Nas-Source",
-    #     cmd='run_command("sudo chmod 777 source-code && sudo ./source-code")')
-    # do(msg="Nas-deb-setup",
-    #     cmd='run_command("sudo chmod 777 nas-build && sudo ./nas-build")')
-    # do(msg="install openmediavault",
-    #     cmd='run_command("sudo apt-get install openmediavault-keyring openmediavault -y")') 
-    # do(msg="Populate the database",
-    #     cmd='run_command("sudo omv-confdbadm populate")')
+        # =============================
+        print("Install dependencies with pip3:")
+        # check whether pip has the option "--break-system-packages"
+        _is_bsps = ''
+        status, _ = run_command("pip3 help install|grep break-system-packages")
+        if status == 0:  # if true
+            _is_bsps = "--break-system-packages"
+            print(
+                "\033[38;5;8m pip3 install with --break-system-packages\033[0m"
+            )
+        # update pip
+        do(msg="update pip3",
+           cmd=f'python3 -m pip install --upgrade pip {_is_bsps}')
+        #
+        for dep in PIP_INSTALL_LIST:
+            do(msg=f"install {dep}", cmd=f'pip3 install {dep} {_is_bsps}')
 
+    # Setup interfaces
+    # =============================
+    print("Setup interfaces")
+    do(msg="turn on SPI", cmd='sudo raspi-config nonint do_spi 0')
 
-        
+    # Install service
+    # =============================
+    print("Install service")
+    do(msg='cp nas-kit service file',
+       cmd='cp ./bin/nas-kit.service /lib/systemd/system/nas-kit.service' +
+       ' && cp ./bin/nas-kit /usr/bin/nas-kit')
+    do(msg='chmod', cmd='chmod +x /usr/bin/nas-kit')
+    do(msg='systemctl daemon-reload', cmd='systemctl daemon-reload')
+    do(msg='enable nas-kit.service', cmd='systemctl enable nas-kit.service')
+    do(msg='start nas-kit', cmd='systemctl start nas-kit')
+
+    # Report error
+    # =============================
     if len(errors) == 0:
-        print("Finished")
+        print("Setup Finished")
     else:
         print("\n\nError happened in install process:")
         for error in errors:
             print(error)
-        print("Try to fix it yourself, or contact service@sunfounder.com with this message")
+        print(
+            "Try to fix it yourself, or contact service@sunfounder.com with this message"
+        )
+        sys.exit(1)
 
-if __name__ =="__main__":
-    install()
+
+if __name__ == "__main__":
+    try:
+        install()
+    except KeyboardInterrupt:
+        print("Canceled.")
+    finally:
+        sys.stdout.write(' \033[1D')
+        sys.stdout.write('\033[?25h')  # cursor visible
+        sys.stdout.flush()
