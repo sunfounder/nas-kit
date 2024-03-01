124802ac822633e1dbd3b6160cad052b13c43121
Lovmong <1967471283@qq.com>
2024-03-01 16:47:05 +0800

    change mothed of getting cpu temp, cpu usgae

--- main/utils.py
+++ main/utils.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import RPi.GPIO as GPIO
 import time
-
+import psutil
 
 FAN_PWM = 18
 LED_PWM = 26
@@ -13,9 +13,9 @@ FAN_MAX = 100
 FAN_MIN = 20
 fan_power = 0
 
-GPIO.setwarnings(False) 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-output_list = [FAN_PWM,LED_PWM]
+output_list = [FAN_PWM, LED_PWM]
 GPIO.setup(output_list, GPIO.OUT)
 
 fan_pwm_pin = GPIO.PWM(FAN_PWM, fan_pwn_freq)
@@ -25,6 +25,7 @@ led_pwm_pin.start(50)
 
 
 class PID():
+
     def __init__(self, P=1, I=1, D=1, expect=0):
         self.P = float(P)
         self.I = float(I)
@@ -64,16 +65,20 @@ class PID():
             result += result_d
         return result
 
+
 #run_command linux
 def run_command(cmd=""):
     import subprocess
-    p = subprocess.Popen(
-        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    p = subprocess.Popen(cmd,
+                         shell=True,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
     result = p.stdout.read().decode('utf-8')
     status = p.poll()
     # print(result)
     # print(status)
     return status, result
+
 
 def do(msg="", cmd=""):
     print(" - %s..." % (msg), end='\r')
@@ -87,52 +92,51 @@ def do(msg="", cmd=""):
         errors.append("%s error:\n  Status:%s\n  Error:%s" %
                       (msg, status, result))
 
-def cpu_temperature():          # cpu_temperature
-    raw_cpu_temperature = subprocess.getoutput("cat /sys/class/thermal/thermal_zone0/temp") 
-    cpu_temperature = round(float(raw_cpu_temperature)/1000,1)               # convert unit
-    cpu_temperature = str(cpu_temperature)
-    return cpu_temperature
 
-def gpu_temperature():          # gpu_temperature(
-    raw_gpu_temperature = subprocess.getoutput( 'vcgencmd measure_temp' )
-    gpu_temperature = round(float(raw_gpu_temperature.replace( 'temp=', '' ).replace( '\'C', '' )), 1)
+def cpu_temperature():  # cpu_temperature
+    cpu_temperature = psutil.sensors_temperatures()['cpu_thermal'][0].current
+    return str(round(cpu_temperature, 1))
+
+
+def gpu_temperature():  # gpu_temperature(
+    raw_gpu_temperature = subprocess.getoutput('vcgencmd measure_temp')
+    gpu_temperature = round(
+        float(raw_gpu_temperature.replace('temp=', '').replace('\'C', '')), 1)
     gpu_temperature = str(gpu_temperature)
     return gpu_temperature
 
-def cpu_usage():                # cpu_usage
-    # result = str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print($2)}'").readline().strip())
-    result = os.popen("mpstat").read().strip()
-    result = result.split('\n')[-1].split(' ')[-1]
-    result = round(100 - float(result), 2)
-    result = str(result)
-    # print(result)
-    return result
 
-def disk_space():               # disk_space
+def cpu_usage():  # cpu_usage
+    return str(round(psutil.cpu_percent(), 2))
+
+
+def disk_space():  # disk_space
     p = os.popen("df -h /")
     i = 0
     while 1:
-        i = i +1
-        line = p.readline()         
-        if i==2:
-            return line.split()[1:5] 
+        i = i + 1
+        line = p.readline()
+        if i == 2:
+            return line.split()[1:5]
+
 
 def portable_hard_disk_info():
     disk_num = os.popen("df -h | grep '/dev/sd' -c")
-    phd = os.popen("df -h | grep '/dev/sd'") 
+    phd = os.popen("df -h | grep '/dev/sd'")
     i = 0
     phd_line = disk_num.readline()
 
     line_list = []
     if int(phd_line) != 0:
         while 1:
-            i = i +1
+            i = i + 1
             line = phd.readline()
-            line_list.append(line.split()[0:6])        
-            if i==int(phd_line):
+            line_list.append(line.split()[0:6])
+            if i == int(phd_line):
                 return line_list
     else:
         return []
+
 
 def ram_info():
     p = os.popen('free')
@@ -140,19 +144,23 @@ def ram_info():
     while 1:
         i = i + 1
         line = p.readline()
-        if i==2:
-            return list(map(lambda x:round(int(x) / 1000,1), line.split()[1:4]))   
+        if i == 2:
+            return list(
+                map(lambda x: round(int(x) / 1000, 1),
+                    line.split()[1:4]))
+
 
 def pi_read():
     result = {
-        "cpu_temperature": cpu_temperature(), 
+        "cpu_temperature": cpu_temperature(),
         "gpu_temperature": gpu_temperature(),
-        "cpu_usage": cpu_usage(), 
-        "disk": disk_space(), 
-        "ram": ram_info(), 
-        # "battery": power_read(), 
+        "cpu_usage": cpu_usage(),
+        "disk": disk_space(),
+        "ram": ram_info(),
+        # "battery": power_read(),
     }
-    return result 
+    return result
+
 
 # def fan_control(temp = 0):
 #     if temp >=68:
@@ -160,18 +168,18 @@ def pi_read():
 #         led_freq = int(temp-67)
 #         if  fan_duty_cycle >= 100:
 #             fan_duty_cycle = 100
-        
+
 #         fan_pwm_pin.ChangeDutyCycle(fan_duty_cycle)
-#         led_pwm_pin.ChangeDutyCycle(100)  
-        
+#         led_pwm_pin.ChangeDutyCycle(100)
+
 #     else:
 #         fan_pwm_pin.ChangeDutyCycle(0)
-#         led_pwm_pin.ChangeDutyCycle(0) 
+#         led_pwm_pin.ChangeDutyCycle(0)
+
 
 def fan_power_read():
     global fan_power
-    return round(fan_power,1)
-
+    return round(fan_power, 1)
 
 
 def getIP(ifaces=['wlan0', 'eth0']):
@@ -188,18 +196,19 @@ def getIP(ifaces=['wlan0', 'eth0']):
             return ipv4
     return False
 
+
 def pid_control():
     global fan_power
     pid = PID(
-        P = 0.5,
-        I = 1,
-        D = 1,
-        expect = 50,
+        P=0.5,
+        I=1,
+        D=1,
+        expect=50,
     )
     dc = 100
     i = 0
     while True:
-        temp = (float(cpu_temperature())+float(gpu_temperature()))/2.0 
+        temp = (float(cpu_temperature()) + float(gpu_temperature())) / 2.0
         # print(temp)
         dc += pid.run(temp, mode="PD")
         dc = min(FAN_MAX, max(FAN_MIN, dc))
@@ -209,7 +218,8 @@ def pid_control():
         fan_pwm_pin.ChangeDutyCycle(dc)
         led_pwm_pin.ChangeDutyCycle(dc)
         i += 1
-        # time.sleep(1)
+        time.sleep(1)
+
 
 # if __name__ == '__main__':
 #     print(portable_hard_disk_info())
